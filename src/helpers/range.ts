84142b84c2e125d07db84d7d241a96cce98844ea
Peter Klavins <paklavins@gmail.com>
2024-04-27 15:09:43 -0600

    Ran husky init?

--- src/helpers/range.ts
+++ src/helpers/range.ts
@@ -1,3 +1,3 @@
 export default function range(max: number, contents = ''): string[] {
-  return Array.from({ length: max }).fill(contents) as string[];
+  return Array.from({ length: max }).fill(contents);
 }
