3f5b36f82ab477792a75e35e60f180937ee1e03d
Peter Klavins <paklavins@gmail.com>
2024-05-02 00:02:28 -0600

    Adds types.

--- src/helpers/calculateDarknessLevels.ts
+++ src/helpers/calculateDarknessLevels.ts
@@ -3,12 +3,12 @@ import { Coordinate } from '../state';
 export default function calculateDarknessLevels(lightSources: Coordinate[], layer: number) {
   const maxLightLevel = 5;
 
-  const getFlooredDistance = (p1, p2) => {
+  const getFlooredDistance = (p1: Coordinate, p2: Coordinate) => {
     const flatDistance = Math.floor(Math.sqrt(Math.abs(p1[0] - p2[0]) ** 2 + Math.abs(p1[1] - p2[1]) ** 2));
     return maxLightLevel - Math.min(maxLightLevel, Math.max(0, flatDistance));
   };
 
-  const darknessArray = [];
+  const darknessArray: { cellKey: string; lightLevel: number }[] = [];
 
   lightSources.forEach((light) => {
     for (let i = light[0] - maxLightLevel; i <= light[0] + maxLightLevel; i++) {
